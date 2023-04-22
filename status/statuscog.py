7919083e839ff1d0f40e27bb341d947d9301f68b
Andrei Ciobănică <37911417+andreiciobanica@users.noreply.github.com>
2023-04-23 00:52:16 +0300

    Update statuscog.py

--- status/statuscog.py
+++ status/statuscog.py
@@ -1,6 +1,7 @@
 from redbot.core import commands
 from redbot.core import Config
 from discord.ext import tasks
+from datetime import datetime
 import discord
 
 class Status(commands.Cog):
@@ -23,21 +24,20 @@ class Status(commands.Cog):
         channelId = await self.config.channelId()
         messageId = await self.config.messageId()
         if channelId == 0 and messageId == 0:
-            self.config.channelId.set(772899841679818753)
             status_channel = self.bot.get_channel(772899841679818753)
             embed=discord.Embed(color=0xe3ee34, url="https://cdn.discordapp.com/attachments/1001319323161346220/1099451162765307984/logofpt.png")
             embed.set_author(name="FPlayT Community | Status", icon_url="https://cdn.discordapp.com/attachments/1001319323161346220/1099451162765307984/logofpt.png")
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1001319323161346220/1099451162765307984/logofpt.png")
-            embed.add_field(name="Server", value="```FPlayT Advanced Roleplay```", inline=False)
+            embed.add_field(name="Server name", value="```FPlayT Advanced Roleplay```", inline=False)
             embed.add_field(name="Server DNS", value="```server.fplayt.ro```", inline=False)
-            embed.add_field(name="Server connect", value="```connect server.fplayt.ro```", inline=False)
             embed.add_field(name="Status", value="```✅ Online```", inline=True)
             embed.add_field(name="Jucători online", value="```921/1024```", inline=True)
             embed.add_field(name="Server uptime", value="```3h 10m```", inline=True)
-            embed.set_footer(text="FPlayT Community •")
-
+            now = datetime.now()
+            embed.set_footer(text=now.strftime("FPlayT Community • %d.%m.%Y, %H:%M:%S"))
             status_message = await status_channel.send(embed=embed)
-            self.config.messageId.set(status_message)
+            await self.config.channelId.set(772899841679818753)
+            await self.config.messageId.set(status_message)
         else:
             print("a")
         
