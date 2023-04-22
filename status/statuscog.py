9bf220996765342b199a27fc7e5b1dbc7b81ffcb
Andrei Ciobănică <37911417+andreiciobanica@users.noreply.github.com>
2023-04-23 01:47:38 +0300

    Update statuscog.py

--- status/statuscog.py
+++ status/statuscog.py
@@ -4,6 +4,7 @@ from discord.ext import tasks
 from datetime import date, datetime, time
 from babel.dates import format_datetime, get_timezone
 import discord
+import requests
 
 class Status(commands.Cog):
     def __init__(self, bot):
@@ -25,6 +26,25 @@ class Status(commands.Cog):
         channelId = await self.config.channelId()
         messageId = await self.config.messageId()
         if channelId == 0 and messageId == 0:
+            serverDetailsRaw = rq.get('http://'+ "185.30.165.128:30120" +'/info.json')
+            serverDetailsJson = serverDetailsRaw.json()
+            serverUptime = "```" + serverDetailsJson["Uptime"] + "```"
+            status_channel = self.bot.get_channel(772899841679818753)
+            embed=discord.Embed(color=0xe3ee34, url="https://cdn.discordapp.com/attachments/1001319323161346220/1099451162765307984/logofpt.png")
+            embed.set_author(name="FPlayT Community | Status", icon_url="https://cdn.discordapp.com/attachments/1001319323161346220/1099451162765307984/logofpt.png")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1001319323161346220/1099451162765307984/logofpt.png")
+            embed.add_field(name="Server name", value="```FPlayT Advanced Roleplay```", inline=False)
+            embed.add_field(name="Server DNS", value="```server.fplayt.ro```", inline=False)
+            embed.add_field(name="Status", value="```✅ Online```", inline=True)
+            embed.add_field(name="Jucători online", value="```921/1024```", inline=True)
+            embed.add_field(name="Server uptime", value=serverUptime, inline=True)
+            dateNow = datetime.now()
+            text = "FPlayT Community • " + str(format_datetime(dateNow, "dd.MM.yyyy HH:mm:ss", tzinfo=get_timezone('Europe/Bucharest'), locale='ro_RO'))
+            embed.set_footer(text=text)
+            status_message = await status_channel.send(embed=embed)
+            await self.config.channelId.set(772899841679818753)
+            await self.config.messageId.set(status_message.id)
+        else:
             status_channel = self.bot.get_channel(772899841679818753)
             embed=discord.Embed(color=0xe3ee34, url="https://cdn.discordapp.com/attachments/1001319323161346220/1099451162765307984/logofpt.png")
             embed.set_author(name="FPlayT Community | Status", icon_url="https://cdn.discordapp.com/attachments/1001319323161346220/1099451162765307984/logofpt.png")
@@ -40,8 +60,6 @@ class Status(commands.Cog):
             status_message = await status_channel.send(embed=embed)
             await self.config.channelId.set(772899841679818753)
             await self.config.messageId.set(status_message.id)
-        else:
-            print("a")
         
     @serverstatus.before_loop
     async def before_printer(self):
