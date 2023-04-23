1780ed488077b158450186e65f74d41e2520ae80
Andrei Ciobănică <37911417+andreiciobanica@users.noreply.github.com>
2023-04-23 03:02:32 +0300

    Update statuscog.py

--- status/statuscog.py
+++ status/statuscog.py
@@ -58,9 +58,8 @@ class Status(commands.Cog):
         else:
             status_channel = self.bot.get_channel(772899841679818753)
             if status_channel is not None:
-                status_message = await status_channel.fetch_message(messageId)
-                print(status_message)
-                if status_message is not None:
+                try:
+                    await status_channel.fetch_message(messageId)
                     serverDetailsJson = requests.get('http://'+ "185.30.165.128:30120" +'/info.json').json()
                     playerNumber = ""
                     serverStatus = ""
@@ -88,7 +87,7 @@ class Status(commands.Cog):
                     text = "FPlayT Community • " + str(format_datetime(dateNow, "dd.MM.yyyy HH:mm:ss", tzinfo=get_timezone('Europe/Bucharest'), locale='ro_RO'))
                     new_embed.set_footer(text=text)
                     await status_message.edit(embed=new_embed)
-                else:
+                except discord.error.NotFound:
                     serverDetailsJson = requests.get('http://'+ "185.30.165.128:30120" +'/info.json').json()
                     playerNumber = ""
                     serverStatus = ""
